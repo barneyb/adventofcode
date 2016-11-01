c9996066a235240f02c04acc83cd16a0d654dd6c
Barney Boisvert <bboisvert@armus.com>
2016-11-01 14:48:46 -0700

    pulled a 'delta_floor' helper function

--- 2015/day1.py
+++ 2015/day1.py
@@ -2,8 +2,12 @@
 import sys
 sys.setrecursionlimit(8000)
 
+def delta_floor(c):
+    return 1 if c == "(" else -1
+    
+
 def find_floor(input):
-    return (1 if input[0] == "(" else -1) + (find_floor(input[1:]) if len(input) > 1 else 0)
+    return delta_floor(input[0]) + (find_floor(input[1:]) if len(input) > 1 else 0)
 
 assert find_floor("(())") == 0
 assert find_floor("()()") == 0
@@ -18,8 +22,7 @@ print find_floor(open("day1_input.txt").read().strip())
 
 def find_floor2(input):
     return reduce(lambda a, b: a + b,
-                  map(lambda c: 1 if c == "(" else -1,
-                      list(input)))
+                  map(delta_floor, list(input)))
 
 assert find_floor2("(())") == 0
 assert find_floor2("()()") == 0
