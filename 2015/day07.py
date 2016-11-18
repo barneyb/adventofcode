b97104e23f0ff92a97295a73306b3de74e212b65
Barney Boisvert <bboisvert@armus.com>
2016-11-17 20:59:33 -0800

    cleaned up the implementation of part two

--- 2015/day07.py
+++ 2015/day07.py
@@ -56,11 +56,7 @@ def get_wire_value(booklet, name):
     return wire_circuit(booklet)[name].output()
 
 def part_two(booklet):
-    wires = wire_circuit(booklet)
-    a = wires['a'].output() 
-    wires = wire_circuit(booklet)
-    wires['b'].output_value = a
-    return wires['a'].output()
+    return get_wire_value(booklet + "\n" + str(get_wire_value(booklet, 'a')) + " -> b", 'a')
 
 class Item:
     
