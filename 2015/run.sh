1a85c42a0773b27eedaff26aadf1633aee112079
Barney Boisvert <bboisvert@armus.com>
2016-11-19 08:41:13 -0800

    fixed emacs' mixed space/tab indentation

--- 2015/run.sh
+++ 2015/run.sh
@@ -12,14 +12,14 @@ for i in `ls *.* | egrep '.*\.(py)$' | sort`; do
     bar
     label $i
     if [ `echo $i | cut -d . -f 2` == "py" ]; then
-	python $i
+        python $i
     else
-	ghc $i && ./`echo $i | cut -d . -f 1`
+        ghc $i && ./`echo $i | cut -d . -f 1`
     fi
     if [ $? != 0 ]; then
         label "FAILURE!"
-	bar
-	break
+    bar
+    break
     fi
     bar
 done
