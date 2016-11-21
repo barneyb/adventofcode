ff7cd3f593d3a18b0208a96ac085b19e84e90417
Barney Boisvert <bboisvert@armus.com>
2016-11-20 21:07:31 -0800

    implemented day one part two in haskell

--- 2015/day01.hs
+++ 2015/day01.hs
@@ -5,6 +5,19 @@ delta ')' = -1
 find_floor :: [Char] -> Int
 find_floor cs = sum (map delta cs)
 
+bfold :: (Int, Int, Int) -> Int -> (Int, Int, Int)
+bfold (p, c, f) d
+    | p >= 0    = (p, 0, 0)
+    | n < 0     = (c + 1, 0, 0)
+    | otherwise = (-1, c + 1, n)
+    where n = f + d
+
+bpos :: [Char] -> Int
+bpos cs =
+    let (r, _, _) = foldl bfold (-1, 0, 0) (map delta cs)
+    in r
+
 main = do
     input <- readFile "day01_input.txt"
     print (find_floor input)
+    print (bpos input)
