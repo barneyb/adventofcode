742b578a745290e35353c7457812c6df4f543ed6
Barney Boisvert <bboisvert@armus.com>
2016-12-05 10:18:33 -0800

    reimplemented part one using character labels instead of integers, and an explict Move type

--- 2016/day02.hs
+++ 2016/day02.hs
@@ -1,26 +1,39 @@
 import Control.Exception (assert)
 
-offset :: Int -> Char -> Int
-offset c 'L'
-    | c `notElem` [1, 4, 7] = c - 1
-offset c 'R'
-    | c `notElem` [3, 6, 9] = c + 1
-offset c 'U'
-    | c `notElem` [1, 2, 3] = c - 3
-offset c 'D'
-    | c `notElem` [7, 8, 9] = c + 3
+data Move = L | R | D | U deriving (Eq, Show)
+
+type Button = Char
+
+parse_move :: Char -> Move
+parse_move 'R' = R
+parse_move 'L' = L
+parse_move 'D' = D
+parse_move 'U' = U
+
+offset :: Button -> Move -> Button
+offset c L
+    | c `notElem` ['1', '4', '7'] = pred c
+offset c R
+    | c `notElem` ['3', '6', '9'] = succ c
+offset c U
+    | c `notElem` ['1', '2', '3'] = pred $ pred $ pred c
+offset c D
+    | c `notElem` ['7', '8', '9'] = succ $ succ $ succ c
 offset c _ = c
 
-start :: Int
-start = 5
+start :: Button
+start = '5'
 
-do_line :: Int -> String -> Int
-do_line c line = foldl offset c line
+do_line :: Button -> String -> Button
+do_line c line = foldl offset c (map parse_move line)
 
 part_one :: String -> String
-part_one input = foldl1 (++) (map show (tail $ scanl do_line start (lines input)))
+part_one input = tail $ scanl do_line start (lines input)
+
+test_input :: String
+test_input = "ULL\nRRDDD\nLURDL\nUUUUD"
 
 main = do
     input <- readFile "day02_input.txt"
-    print $ assert ("1985" == (part_one "ULL\nRRDDD\nLURDL\nUUUUD")) "test one passed!"
+    print $ assert ("1985" == (part_one test_input)) "test one passed!"
     print $ assert ("61529" == (part_one input)) "part one passed!"
