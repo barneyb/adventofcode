37f296e56b58081f9da68e62eda801786cded3bb
Barney Boisvert <bboisvert@armus.com>
2016-12-05 11:19:30 -0800

    implemented day two part two

--- 2016/day02.hs
+++ 2016/day02.hs
@@ -10,30 +10,74 @@ parse_move 'L' = L
 parse_move 'D' = D
 parse_move 'U' = U
 
-offset :: Button -> Move -> Button
-offset c L
+{-
+1 2 3
+4 5 6
+7 8 9
+-}
+move1 :: Button -> Move -> Button
+move1 c L
     | c `notElem` ['1', '4', '7'] = pred c
-offset c R
+move1 c R
     | c `notElem` ['3', '6', '9'] = succ c
-offset c U
+move1 c U
     | c `notElem` ['1', '2', '3'] = pred $ pred $ pred c
-offset c D
+move1 c D
     | c `notElem` ['7', '8', '9'] = succ $ succ $ succ c
-offset c _ = c
+move1 c _ = c
+
+{-
+    1
+  2 3 4
+5 6 7 8 9
+  A B C
+    D
+-}
+move2 :: Button -> Move -> Button
+move2 c d = (moves c) !! (idx d)
+    where
+        idx :: Move -> Int
+        idx L = 0
+        idx R = 1
+        idx D = 2
+        idx U = 3
+        moves :: Button -> [Button]
+                  --  L    R    D    U
+        moves '1' = ['1', '1', '3', '1']
+        moves '2' = ['2', '3', '6', '2']
+        moves '3' = ['2', '4', '7', '1']
+        moves '4' = ['3', '4', '8', '4']
+        moves '5' = ['5', '6', '5', '5']
+        moves '6' = ['5', '7', 'A', '2']
+        moves '7' = ['6', '8', 'B', '3']
+        moves '8' = ['7', '9', 'C', '4']
+        moves '9' = ['8', '9', '9', '9']
+        moves 'A' = ['A', 'B', 'A', '6']
+        moves 'B' = ['A', 'C', 'D', '7']
+        moves 'C' = ['B', 'C', 'C', '8']
+        moves 'D' = ['D', 'D', 'D', 'B']
 
 start :: Button
 start = '5'
 
-do_line :: Button -> String -> Button
-do_line c line = foldl offset c (map parse_move line)
+do_line :: (Button -> Move -> Button) -> Button -> String -> Button
+do_line m c line = foldl m c (map parse_move line)
+
+do_code :: (Button -> Move -> Button) -> String -> String
+do_code m s = tail $ scanl (do_line m) start (lines s)
 
 part_one :: String -> String
-part_one input = tail $ scanl do_line start (lines input)
+part_one = do_code move1
 
 test_input :: String
 test_input = "ULL\nRRDDD\nLURDL\nUUUUD"
+
+part_two :: String -> String
+part_two = do_code move2
 
 main = do
     input <- readFile "day02_input.txt"
     print $ assert ("1985" == (part_one test_input)) "test one passed!"
     print $ assert ("61529" == (part_one input)) "part one passed!"
+    print $ assert ("5DB3" == (part_two test_input)) "test two passed!"
+    print $ assert ("C2C28" == (part_two input)) "test two passed!"
