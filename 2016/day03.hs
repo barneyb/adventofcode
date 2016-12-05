5f559bc3fb6bade885532036881789cfd6fb21e7
Barney Boisvert <bboisvert@armus.com>
2016-12-05 14:03:41 -0800

    much improved the parsing with some function composition

--- 2016/day03.hs
+++ 2016/day03.hs
@@ -1,13 +1,13 @@
 import Control.Exception (assert)
 import qualified Data.List as L
 
-to_lines :: String -> [[Int]]
-to_lines input = map L.sort (map (map read) (map words (lines input)))
+parse_line :: String -> [Int]
+parse_line input = L.sort (map read (words input))
 
 parse :: String -> [(Int, Int, Int)]
-parse input = map (\[a, b, c] -> (a, b, c)) (to_lines input)
+parse input = map ((\[a, b, c] -> (a, b, c)) . parse_line) (lines input)
 
---part_one :: String -> Int
+part_one :: String -> Int
 part_one input = length $ filter (\(a, b, c) -> a + b > c) (parse input)
 
 main = do
