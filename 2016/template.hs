5385c2f01c4448c03d266b81f9c3f01d55e03f17
Barney Boisvert <bboisvert@armus.com>
2016-12-06 19:21:09 -0800

    commented out the "part two" stuff in the template

--- 2016/template.hs
+++ 2016/template.hs
@@ -3,12 +3,12 @@ import Control.Exception (assert)
 part_one :: String -> Int
 part_one input = length input
 
-part_two :: String -> Int
-part_two input = length input
+--part_two :: String -> Int
+--part_two input = length input
 
 main = do
     input <- readFile "day_input.txt"
     print (part_one input)
     print $ assert (0 == (part_one input)) "part one passed!"
-    print (part_two input)
-    print $ assert (0 == (part_two input)) "part one passed!"
+--    print (part_two input)
+--    print $ assert (0 == (part_two input)) "part one passed!"
