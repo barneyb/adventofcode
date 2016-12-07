264578a4f5f01b5ce5b7f456d6be7c32eaf9b90b
Barney Boisvert <bboisvert@armus.com>
2016-12-07 08:32:10 -0800

    implemented day five part two

--- 2016/day05.hs
+++ 2016/day05.hs
@@ -21,22 +21,38 @@ pass len key =
 part_one :: String -> String
 part_one = pass 8
 
---part_two :: String -> String
---part_two input = length input
+-- a la the one for Seq
+update :: Int -> a -> [a] -> [a]
+update i x xs = take i xs ++ (x : drop (i + 1) xs)
+
+part_two :: String -> String
+part_two key =
+    let dpairs = map (\c -> (c !! 5, c !! 6)) (candidates key)
+        cpairs = filter (\ c -> ((fst c) `elem` ['0'..'7'])) dpairs
+        pairs = map (\ (p, c) -> (read (p:[]), c)) cpairs
+        m :: String -> (Int, Char) -> String
+        m str (i, c)
+            | str !! i /= '_' = str
+            | otherwise       = update i c str
+    in head $ dropWhile ('_' `elem`) (scanl m "________" pairs)
 
 test_input = "abc"
 input = "reyedfim"
 
 main = do
-    print $ assert ("1" == (pass 1 test_input)) "test one-1 passed!"
-    print $ assert ("18" == (pass 2 test_input)) "test one-2 passed!"
-    print $ assert ("18f" == (pass 3 test_input)) "test one-3 passed!"
-    print $ assert ("18f4" == (pass 4 test_input)) "test one-4 passed!"
-    print $ assert ("18f47" == (pass 5 test_input)) "test one-5 passed!"
-    print $ assert ("18f47a" == (pass 6 test_input)) "test one-6 passed!"
-    print $ assert ("18f47a3" == (pass 7 test_input)) "test one-7 passed!"
-    print $ assert ("18f47a30" == (pass 8 test_input)) "test one-8 passed!"
-    print $ assert ("18f47a30" == (part_one test_input)) "test one passed!"
-    print $ assert ("f97c354d" == (part_one input)) "part one passed!"
---    print (part_two input)
---    print $ assert (0 == (part_two input)) "part one passed!"
+--    print $ assert ("1" == (pass 1 test_input)) "test one-1 passed!"
+--    print $ assert ("18" == (pass 2 test_input)) "test one-2 passed!"
+--    print $ assert ("18f" == (pass 3 test_input)) "test one-3 passed!"
+--    print $ assert ("18f4" == (pass 4 test_input)) "test one-4 passed!"
+--    print $ assert ("18f47" == (pass 5 test_input)) "test one-5 passed!"
+--    print $ assert ("18f47a" == (pass 6 test_input)) "test one-6 passed!"
+--    print $ assert ("18f47a3" == (pass 7 test_input)) "test one-7 passed!"
+--    print $ assert ("18f47a30" == (pass 8 test_input)) "test one-8 passed!"
+--    print $ assert ("18f47a30" == (part_one test_input)) "test one passed!"
+--    print $ assert ("f97c354d" == (part_one input)) "part one passed!"
+--    let h = part_two test_input
+--    print h
+--    print $ assert ("05ace8e3" == h) "test two passed!"
+    let h = part_two input
+    print h
+    print $ assert ("863dde27" == h) "part two passed!"
