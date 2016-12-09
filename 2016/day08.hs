8e189edef87435e0ed8a1dfc15e92069a857a616
Barney Boisvert <bboisvert@armus.com>
2016-12-08 20:14:30 -0800

    consolidated on zero-indexing, finished draw, and implemented execute

--- 2016/day08.hs
+++ 2016/day08.hs
@@ -1,5 +1,6 @@
 import Control.Exception (assert)
 import Data.Array
+import qualified Data.Text as T
 import Utils
 
 data Cmd = Rect Int Int | Row Int Int | Col Int Int
@@ -11,13 +12,23 @@ type Pixel = (Point, Bool)
 type Display = Array Point Bool
 
 box :: Int -> Int -> Bool -> [Pixel]
-box w h v = [ ((x, y), v) | x <- [1..w], y <- [1..h] ]
+box w h v = [ ((x, y), v) | x <- [0..(w-1)], y <- [0..(h-1)] ]
 
 build :: Int -> Int -> Display
-build w h = array ( (1, 1), (w, h) ) (box w h False)
+build w h = array ( (0, 0), (w-1, h-1) ) (box w h False)
 
 execute :: Display -> Cmd -> Display
 execute d (Rect w h) = d//(box w h True)
+execute d (Row y n) =
+    let ((_, _), (w, h)) = bounds d
+        ps = filter (\((_, y'), _) -> y == y') (assocs d)
+        ps' = map (\((x, y), v) -> (((x+n) `mod` (w+1), y), v)) ps
+    in d//ps'
+execute d (Col x n) =
+    let ((_, _), (w, h)) = bounds d
+        ps = filter (\((x', _), _) -> x == x') (assocs d)
+        ps' = map (\((x, y), v) -> ((x, (y+n) `mod` (h+1)), v)) ps
+    in d//ps'
 
 parse :: String -> Cmd
 parse s = Rect 1 1
@@ -28,7 +39,9 @@ count_lit d = 0
 draw :: Display -> [String]
 draw d =
     let ((_, _), (w, h)) = bounds d
-    in take h $ repeat (take w $ repeat '.')
+        ls = concat $ take (h+1) $ repeat (take (w+1) $ repeat '.')
+        ls' = foldl (\ls ((x, y), _) -> update ((w+1) * y + x) '#' ls) ls (filter snd (assocs d))
+    in map T.unpack (T.chunksOf (w+1) (T.pack ls'))
 
 write :: Display -> IO ()
 write d = prints $ draw d
@@ -44,22 +57,27 @@ part_one = do_part_one 50 6
 
 main = do
     input <- readFile "day08_input.txt"
+    let d = foldl execute (build 7 3) [(Rect 1 3), (Rect 3 2), (Rect 6 1)]
+    write d
+    print $ assert (["######.",
+                     "###....",
+                     "#......"] == (draw d)) "step two passed!"
     let d = execute (build 7 3) (Rect 3 2)
     write d
     print $ assert (["###....",
                      "###....",
                      "......."] == (draw d)) "step one passed!"
-    let d = foldl execute (build 7 3) [(Rect 3 2), (Col 2 1)]
+    let d = foldl execute (build 7 3) [(Rect 3 2), (Col 1 1)]
     write d
     print $ assert (["#.#....",
                      "###....",
                      ".#....."] == (draw d)) "step two passed!"
-    let d = foldl execute (build 7 3) [(Rect 3 2), (Col 2 1), (Row 1 4)]
+    let d = foldl execute (build 7 3) [(Rect 3 2), (Col 1 1), (Row 0 4)]
     write d
     print $ assert (["....#.#",
                      "###....",
                      ".#....."] == (draw d)) "step three passed!"
-    let d = foldl execute (build 7 3) [(Rect 3 2), (Col 2 1), (Row 1 4), (Col 2 1)]
+    let d = foldl execute (build 7 3) [(Rect 3 2), (Col 1 1), (Row 0 4), (Col 1 1)]
     write d
     print $ assert ([".#..#.#",
                      "#.#....",
