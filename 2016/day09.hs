b270aa34f687ad848f320869d2e5ec382447707f
Barney Boisvert <bboisvert@armus.com>
2016-12-09 09:14:50 -0800

    impemented what appears to be a correct soltuion for day nine part two, though it's too slow to actually do the processing

--- 2016/day09.hs
+++ 2016/day09.hs
@@ -4,50 +4,81 @@ import Text.Regex.TDFA
 import Utils
 
 parse :: String -> String
-parse s
+parse = gparse False
+
+parseR :: String -> String
+parseR = gparse True
+
+gparse :: Bool -> String -> String
+gparse r s
     | '(' `elem` s  =
         let (prefix, _, suffix, ps) = s =~ "\\(([0-9]+)x([0-9]+)\\)" :: (String, String, String, [String])
             ns = map read ps
             hs = splitAt (ns !! 0) suffix
-        in prefix ++ (concat $ take (ns !! 1) $ repeat (fst hs)) ++ (parse (snd hs))
+            mid = concat $ take (ns !! 1) $ repeat (fst hs)
+        in prefix ++ (if r then (parseR (mid ++ (snd hs))) else (mid ++ (parse (snd hs))))
     | otherwise     = s
 
-part_one :: String -> Int
-part_one input = length $ filter (not . C.isSpace) (parse input)
+no_spaces :: String -> String
+no_spaces = filter (not . C.isSpace)
 
---part_two :: String -> Int
---part_two input = length input
+part_one :: String -> Int
+part_one = length . parse . no_spaces
+
+part_two :: String -> Int
+part_two = length . parseR . no_spaces
 
 main = do
     input <- readFile "day09_input.txt"
 
     let r = parse "ADVENT"
     print r
-    print $ assert ("ADVENT" == r) "test one passed"
+    print $ assert ("ADVENT" == r) "test 1.one passed"
 
     let r = parse "A(1x5)BC"
     print r
-    print $ assert ("ABBBBBC" == r) "test two passed"
+    print $ assert ("ABBBBBC" == r) "test 1.two passed"
 
     let r = parse "(3x3)XYZ"
     print r
-    print $ assert ("XYZXYZXYZ" == r) "test three passed"
+    print $ assert ("XYZXYZXYZ" == r) "test 1.three passed"
 
     let r = parse "A(2x2)BCD(2x2)EFG"
     print r
-    print $ assert ("ABCBCDEFEFG" == r) "test four passed"
+    print $ assert ("ABCBCDEFEFG" == r) "test 1.four passed"
 
     let r = parse "(6x1)(1x3)A"
     print r
-    print $ assert ("(1x3)A" == r) "test  five passed"
+    print $ assert ("(1x3)A" == r) "test 1.five passed"
 
     let r = parse "X(8x2)(3x3)ABCY"
     print r
-    print $ assert ("X(3x3)ABC(3x3)ABCY" == r) "test  passed"
+    print $ assert ("X(3x3)ABC(3x3)ABCY" == r) "test 1.six passed"
+
+    let r = parseR "(3x3)XYZ"
+    print r
+    print $ assert ("XYZXYZXYZ" == r) "test 2.1 passed"
+
+    let r = parseR "X(8x2)(3x3)ABCY"
+    print r
+    print $ assert ("XABCABCABCABCABCABCY" == r) "test 2.2 passed"
+
+    let r = parseR "X(14x2)(8x2)(2x3)ABCY"
+    print r
+    print $ assert ("XABABABCABABABCYABABABCABABABCY" == r) "test 2.beb.1 passed"
+
+    let r = part_two "(27x12)(20x12)(13x14)(7x10)(1x12)A"
+    print r
+    print $ assert (241920 == r) "test 2.3 passed"
+
+    let r = parseR "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+    print r
+    print $ assert ("ABCABCABCXYXYXYPQRSTPQRSTABCABCABCXYXYXYPQRSTPQRSTABCABCABCXYXYXYPQRSTPQRSTXTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVENTWOTWOSEVENSEVENSEVENSEVENSEVENSEVENSEVEN" == r) "test 2.4 passed"
 
     let r = part_one input
     print r
     print $ assert (183269 == r) "part one passed!"
+
 --    let r = part_two input
 --    print r
 --    print $ assert (0 == r) "part two passed!"
