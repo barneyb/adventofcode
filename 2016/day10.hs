af8333150f945cb989bace32609353bce0c70507
Barney Boisvert <bboisvert@armus.com>
2016-12-13 10:04:59 -0800

    tried to separate loading, but no dice.

--- 2016/day10.hs
+++ 2016/day10.hs
@@ -7,21 +7,15 @@ type Value = Int
 
 type Id = Int
 
-data Cmd = Load Value Id | Pass Id Id Id | Dump Id Id Id | PassDump Id Id Id | DumpPass Id Id Id deriving (Eq, Show)
-
-load_regex :: String
-load_regex = "value ([0-9]+) goes to bot ([0-9]+)"
-
-pass_regex :: String
-pass_regex = "bot ([0-9]+) gives low to (bot|output) ([0-9]+) and high to (bot|output) ([0-9]+)"
+data Cmd = Load Value Id | Pass Id Id Id | PassDump Id Id Id | DumpPass Id Id Id | Dump Id Id Id deriving (Eq, Show, Ord)
 
 parse :: String -> Cmd
 parse s
     | "value" `L.isPrefixOf` s =
-        let [v, b] = map read (regexgrps s load_regex)
+        let [v, b] = map read (regexgrps s "value ([0-9]+) goes to bot ([0-9]+)")
         in Load v b
     | otherwise                =
-        let ps = regexgrps s pass_regex
+        let ps = regexgrps s "bot ([0-9]+) gives low to (bot|output) ([0-9]+) and high to (bot|output) ([0-9]+)"
             ds = (ps!!1, ps!!3)
             is = map read [ps!!0, ps!!2, ps!!4]
         in p ds is
@@ -32,8 +26,22 @@ parse s
             p ("output", "bot") [a,x,y] = DumpPass a x y
             p ("output", "output") [a,x,y] = Dump a x y
 
-part_one :: String -> Int
-part_one input = length input
+loaded_into :: [Cmd] -> Value -> Id
+loaded_into cs v =
+    let Just c = lookup v $ map (\(Load v' i) -> (v', i)) cs
+    in c
+
+pLoadMove :: [Cmd] -> ([Cmd], [Cmd])
+pLoadMove = L.partition p
+    where
+        p :: Cmd -> Bool
+        p (Load _ _) = True
+        p _ = False
+
+part_one :: String -> Int -> Int -> Int
+part_one input l h =
+    let (ls, ps) = pLoadMove $ map parse (lines input)
+    in loaded_into ls l
 
 --part_two :: String -> Int
 --part_two input = length input
@@ -49,18 +57,38 @@ test_input =
 main = do
     input <- readFile "day10_input.txt"
 
-    print $ parse "value 0 goes to bot 2"
-    print $ parse "bot 3 gives low to bot 4 and high to bot 5"
-    print $ parse "bot 6 gives low to output 7 and high to bot 8"
-    print $ parse "bot 9 gives low to output 10 and high to output 11"
-    print $ parse "bot 12 gives low to bot 13 and high to output 14"
-
-    print $ map parse (lines test_input)
-    print $ map parse (lines input)
-
-    let r = part_one input
+    let r = parse "value 0 goes to bot 2"
     print r
-    print $ assert (0 == r) "part one passed!"
+    print $ assert (Load 0 2 == r) "parse load passed"
+
+    let r = parse "bot 3 gives low to bot 4 and high to bot 5"
+    print r
+    print $ assert (Pass 3 4 5 == r) "parse pass passed"
+
+    let r = parse "bot 6 gives low to output 7 and high to bot 8"
+    print r
+    print $ assert (DumpPass 6 7 8 == r) "parse dump pass passed"
+
+    let r = parse "bot 9 gives low to output 10 and high to output 11"
+    print r
+    print $ assert (Dump 9 10 11 == r) "parse dump passed"
+
+    let r = parse "bot 12 gives low to bot 13 and high to output 14"
+    print r
+    print $ assert (PassDump 12 13 14 == r) "parse pass dump passed"
+
+    let r = part_one test_input 2 5
+    print r
+    print $ assert (2 == r) "test one passed!"
+
+    let r = part_one test_input 2 3
+    print r
+    print $ assert (1 == r) "test two passed!"
+
+--     let r = part_one input 17 61
+--     print r
+--     print $ assert (0 == r) "part one passed!"
+
 --    let r = part_two input
 --    print r
 --    print $ assert (0 == r) "part two passed!"
