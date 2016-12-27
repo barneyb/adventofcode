c7142cfec3275934bafdcd6be1a88103e6d2b766
Barney Boisvert <bboisvert@armus.com>
2016-12-26 17:59:16 -0800

    day thirteen part one

--- 2016/day13.hs
+++ 2016/day13.hs
@@ -1,27 +1,43 @@
 import Control.Exception (assert)
 import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
+import qualified Data.HashSet as S
 import Utils
 
 type Key = Int
+
 type Point = (Int, Int)
+
+type Generation = (Int, S.HashSet Point)
 
 as_binary :: Int -> String
 as_binary n = showIntAtBase 2 intToDigit n ""
 
 is_open :: Key -> Point -> Bool
-is_open k (x, y) =
-    let n = x*x + 3*x + 2*x*y + y + y*y
-        s = as_binary (n + k)
-        c = length $ filter (== '1') s
-    in even c
+is_open k (x, y)
+    | x < 0 || y < 0 = False
+    | otherwise      =
+        let n = x*x + 3*x + 2*x*y + y + y*y
+            s = as_binary (n + k)
+            c = length $ filter (== '1') s
+        in even c
 
 draw :: Key -> Point -> Point -> [String]
 draw k (x, y) (x', y') = map (\r ->
     map (\c -> if is_open k (c, r) then '.' else '#') [x..x']) [y..y']
 
+step :: Key -> Point -> S.HashSet Point
+step key (x, y) = S.fromList $ filter (is_open key) [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+
+step_count :: Key -> Point -> Point -> Int
+step_count key start dest =
+    let gens = scanl (\(_, ps) n -> (n, S.unions (map (step key) (S.toList ps)))) (0, S.singleton start) [1..]
+        gen = head $ dropWhile (\g -> not (S.member dest (snd g))) gens
+    in fst gen
+
 part_one :: Int -> Int
-part_one input = input
+part_one input = step_count input (1, 1) (31, 39)
+
 
 --part_two :: Int -> Int
 --part_two input = length input
@@ -48,9 +64,13 @@ main = do
 
     prints $ draw input (0, 0) (40, 50)
 
+    let r = step_count 10 (1, 1) (7, 4)
+    print r
+    print $ assert (11 == r) "example one passed!"
+
     let r = part_one input
     print r
-    print $ assert (0 == r) "part one passed!"
+    print $ assert (86 == r) "part one passed!"
 --     let r = part_two input
 --     print r
 --     print $ assert (0 == r) "part two passed!"
