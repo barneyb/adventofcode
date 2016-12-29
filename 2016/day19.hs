2b89d0b09c291d36a42b562f6da67da57674a62c
Barney Boisvert <bboisvert@armus.com>
2016-12-28 16:03:27 -0800

    day nineteen part two

--- 2016/day19.hs
+++ 2016/day19.hs
@@ -12,8 +12,22 @@ part_one n = go (S.fromList [1..n])
             | otherwise         = go ((S.drop 2 s) S.|> tip)
             where tip = s `S.index` 0
 
---part_two :: String -> Int
---part_two input = length input
+-- my 'containers' package is too old to have this
+deleteAt :: Int -> S.Seq a -> S.Seq a
+deleteAt i s = S.take i s S.>< S.drop (i+1) s
+
+part_two :: Int -> Int
+part_two n = go (S.fromList [1..n])
+     where
+         go :: S.Seq Int -> Int
+         go s
+             | len == 1  = tip
+             | otherwise =
+                let s' = deleteAt (len `div` 2) s
+                in go ((S.drop 1 s') S.|> tip)
+             where
+                len = S.length s
+                tip = s `S.index` 0
 
 main = do
     assert_equal 3 (part_one 5) "example one"
@@ -23,4 +37,6 @@ main = do
 
     assert_equal 1816277 (part_one 3005290) "part one"
 
---     assert_equal 0 (part_two 3005290) "part two"
+    assert_equal 2 (part_two 5) "example two"
+
+    assert_equal 1410967 (part_two 3005290) "part two"
